94f05829bc1764ca343c43ce3c93e40647418b9a
Christian Hegedüs <christian.hegedues@paessler.com>
2025-08-21 12:25:56 +0200

    nvim: update tokyonight plugin

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -35,7 +35,6 @@ require('lazy').setup({
 
   require 'kickstart.plugins.blink-cmp',
 
-  require 'kickstart.plugins.tokyonight',
 
   require 'kickstart.plugins.todo-comments',
 
@@ -43,6 +42,7 @@ require('lazy').setup({
 
   require 'kickstart.plugins.treesitter',
 	--]]
+  require 'plugins.tokyonight',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
