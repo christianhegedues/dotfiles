0d824ed187ae2faa1c3abcaa14105c26c2edfe41
Christian Hegedüs <christian.hegedues@paessler.com>
2025-08-21 11:58:28 +0200

    nvim: better plugin handling

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,25 +16,26 @@ require('keymaps')
 -- See `:help lazy.nvim.txt`
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  })
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
 end
 
--- Add lazy to the `runtimepath`, this allows us to `require` it.
---@diagnostic disable-next-line; undefined-field
-vim.opt.rtp:prepend(lazypath)
+--@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
 
+require('lazy-plugins')
+
+--[[
 require('lazy').setup({ import = 'plugins' }, {
   change_detection = {
     notify = false,
   },
 })
+--]]
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
